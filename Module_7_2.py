51a564ac9305cbe171c2342b1dd9c870ee0f3d2f
IRAKLY <lagutin.irakly@yandex.ru>
2024-11-30 16:40:54 +0300

    Add files via upload

---
+++ Module_7_2.py
@@ -0,0 +1,21 @@
+def custom_write(file_name, strings):
+    strings_positions = {}
+    file = open(file_name, 'w', encoding="utf-8")
+    for i, string in enumerate(strings, 1):
+        start_string = file.tell()
+        file.write(string + '\n')
+        strings_positions[i, start_string] = string
+
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
